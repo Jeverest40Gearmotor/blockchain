fbfc84293745202044f828599eac50eea9187040
Jeremiah Short <jeremiah.short@mrjaffesclass.com>
2023-04-19 12:56:45 -0700

    Finished added cryptocurrency

--- Blockchain.js
+++ Blockchain.js
@@ -165,4 +165,23 @@ class Transaction {
     }
 }
 
-module.exports = {Block, Blockchain};
+module.exports = {Block, Blockchain, Transaction};
+
+const JzChain = new Blockchain();
+
+// Your original balance is 100000
+
+const girlfriendWallet = ec.genKeyPair();
+
+// Create a transaction
+const transaction = new Transaction(holderKeyPair.getPublic("hex"), girlfriendWallet.getPublic("hex"), 100, 10);
+// Sign the transaction
+transaction.sign(holderKeyPair);
+// Add transaction to pool
+JzChain.addTransaction(transaction);
+// Mine transaction
+JzChain.mineTransactions(holderKeyPair.getPublic("hex"));
+
+// Prints out balance of both address
+console.log("Your balance:", JzChain.getBalance(holderKeyPair.getPublic("hex")));
+console.log("Your girlfriend's balance:", JzChain.getBalance(girlfriendWallet.getPublic("hex")));
--- JzChain.js
+++ JzChain.js
@@ -1,9 +1,23 @@
-const { Block, Blockchain } = require("./Blockchain.js");
+const { Block, Blockchain, Transaction } = require("./Blockchain.js");
+const EC = require("elliptic").ec, ec = new EC("secp256k1");
 
 const JzChain = new Blockchain();
-// Add a new block
-JzChain.addBlock(new Block(Date.now().toString(), { from: "John", to: "Bob", amount: 100 }));
-// (This is just a fun example, real cryptocurrencies often have some more steps to implement).
+// Your original balance is 100000
 
+const girlfriendWallet = ec.genKeyPair();
+const holderKeyPair = ec.genKeyPair();
+
+// Create a transaction
+const transaction = new Transaction(holderKeyPair.getPublic("hex"), girlfriendWallet.getPublic("hex"), 100, 10);
+// Sign the transaction
+transaction.sign(holderKeyPair);
+// Add transaction to pool
+JzChain.addTransaction(transaction);
+// Mine transaction
+JzChain.mineTransactions(holderKeyPair.getPublic("hex"));
+
+// Prints out balance of both address
+console.log("Your balance:", JzChain.getBalance(holderKeyPair.getPublic("hex")));
+console.log("Your girlfriend's balance:", JzChain.getBalance(girlfriendWallet.getPublic("hex")));
 // Prints out the updated chain
-console.log(JzChain.chain); 
+//console.log(JzChain.chain);
